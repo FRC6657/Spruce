bf92736be3bd945f74ecc6e48a1e0c903aab6851
Addie R <addison.reardon@uphsconnect.org>
2022-10-24 18:38:09 -0700

    more clean up

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
  
   @Override
   public void robotInit() {
@@ -17,13 +16,11 @@ public class Robot extends TimedRobot {
     m_robotContainer = new RobotContainer();
   }
 
-
   @Override
   public void robotPeriodic() {
     
     CommandScheduler.getInstance().run();
   }
-
 
   @Override
   public void disabledInit() {}
@@ -31,54 +28,39 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
-  public void autonomousInit() {
+  public void autonomousInit() {}
 
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
+  
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+
   }
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-
-
 
   }
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
+    
     CommandScheduler.getInstance().cancelAll();
-  }
 
-  /** This function is called periodically during test mode. */
+  }
+  
   @Override
   public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
 }
